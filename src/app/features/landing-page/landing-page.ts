f9dfa1cf265bf4f4b069354c3be47a70f4a76c09
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-05-03 02:31:20 +0200

    Landing page layout created.

--- src/app/features/landing-page/landing-page.ts
+++ src/app/features/landing-page/landing-page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { ThemeToggle } from './components/theme-toggle/theme-toggle';
-import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzButtonModule, NzButtonSize } from 'ng-zorro-antd/button';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [ThemeToggle, NzButtonModule],
+  imports: [NzButtonModule, Header],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
-export class LandingPage {}
+export class LandingPage {
+  size: NzButtonSize = 'large';
+}
