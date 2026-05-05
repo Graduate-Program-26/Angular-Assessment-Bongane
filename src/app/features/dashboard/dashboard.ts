076ab79aeacf86a5d670135dd0e3618bd6ea2737
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-05-05 10:04:30 +0200

    Reafactored dasboard search components into their own files.

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,35 +1,18 @@
-import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
-import { NzButtonModule } from 'ng-zorro-antd/button';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzPopoverModule } from 'ng-zorro-antd/popover';
 import { FormsModule } from '@angular/forms';
 import { SearchService } from '../../services/search-service';
-import {
-  CdkFixedSizeVirtualScroll,
-  CdkVirtualForOf,
-  CdkVirtualScrollViewport,
-} from '@angular/cdk/scrolling';
 import { NzListModule } from 'ng-zorro-antd/list';
-import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
 import { SearchItem } from '../../models/search-item.model';
 import { debounceTime, distinctUntilChanged, Subject, switchMap, tap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { SearchResultsCard } from '../search/search-results-card/search-results-card';
+import { SearchInput } from '../search/search-input/search-input';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [
-    NzButtonModule,
-    NzInputModule,
-    NzIconModule,
-    FormsModule,
-    CdkVirtualScrollViewport,
-    CdkFixedSizeVirtualScroll,
-    CdkVirtualForOf,
-    NzListModule,
-    NzSkeletonModule,
-    NzPopoverModule,
-  ],
+  imports: [NzInputModule, NzIconModule, FormsModule, NzListModule, SearchResultsCard, SearchInput],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
@@ -41,25 +24,9 @@ export class Dashboard {
   offset = signal(0);
   isLoading = signal(false);
   nextUrl = signal<string | null>(null);
-  visible = false;
-
-  activePopoverId = signal<number | null>(null);
-
-  togglePopover(id: number) {
-    this.activePopoverId.set(this.activePopoverId() === id ? null : id);
-  }
-
-  change(value: boolean) {}
-
-  closePopover(id: number) {
-    if (this.activePopoverId() === id) {
-      this.activePopoverId.set(null);
-    }
-  }
 
   private readonly searchService = inject(SearchService);
-  private readonly destroyRef = inject(DestroyRef);
-  private searchSubject = new Subject<string>();
+  protected searchSubject = new Subject<string>();
 
   private readonly searchSubsciption = this.searchSubject
     .pipe(
@@ -88,41 +55,4 @@ export class Dashboard {
         this.isLoading.set(false);
       },
     });
-
-  onSearchChange(query: string) {
-    this.searchSubject.next(query);
-  }
-
-  trackById(index: number, item: SearchItem) {
-    return item.id;
-  }
-
-  onScrolledIndexChange(index: number) {
-    const nearEnd = index >= this.searchItems().length - 13;
-    if (nearEnd && !this.isLoading() && this.hasMore()) {
-      this.loadMore();
-    }
-  }
-
-  private loadMore() {
-    const next = this.nextUrl();
-    if (!next) return;
-
-    this.isLoading.set(true);
-    this.searchService
-      .searchNext(next)
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe({
-        next: (res) => {
-          this.searchItems.update((items) => [...items, ...res.data]);
-          this.nextUrl.set(res.next);
-          this.hasMore.set(!!res.next);
-          this.isLoading.set(false);
-        },
-        error: (err) => {
-          this.error.set(err.message);
-          this.isLoading.set(false);
-        },
-      });
-  }
 }
