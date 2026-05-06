7900365a1f9e796456ff8f4cb3d177424932a383
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-05-06 23:26:09 +0200

    Added cdkVirtualFor for performance gains

--- src/app/features/playlist/playlist-page/playlist-page.ts
+++ src/app/features/playlist/playlist-page/playlist-page.ts
@@ -2,27 +2,28 @@ import { Component, computed, DestroyRef, inject, model } from '@angular/core';
 import { NzImageModule } from 'ng-zorro-antd/image';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { NzTableModule } from 'ng-zorro-antd/table';
-import { SearchItemActions } from '../../search/search-item-actions/search-item-actions';
-import { NzSkeletonComponent } from 'ng-zorro-antd/skeleton';
-import { NzCheckListModule, NzItemProps } from 'ng-zorro-antd/check-list';
+import { NzCheckListModule } from 'ng-zorro-antd/check-list';
 import {
   CdkFixedSizeVirtualScroll,
   CdkVirtualForOf,
   CdkVirtualScrollViewport,
 } from '@angular/cdk/scrolling';
-import { SearchService } from '../../../services/search-service';
-import { SearchItem } from '../../../models/search-item.model';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { PlaylistStore } from '../store/playlist.store';
-import { NgOptimizedImage } from '@angular/common';
-import { PlaylistService } from '../../../services/playlist-service';
 import { ActivatedRoute } from '@angular/router';
-import { Playlist } from '../../../models/playlist.model';
 import { Track } from '../../../models/track.model';
+import { PlaylistTrack } from '../../../models/playlist.model';
 
 @Component({
   selector: 'app-playlist-page',
-  imports: [NzImageModule, NzListModule, NzTableModule, NzCheckListModule],
+  imports: [
+    NzImageModule,
+    NzListModule,
+    NzTableModule,
+    NzCheckListModule,
+    CdkFixedSizeVirtualScroll,
+    CdkVirtualForOf,
+    CdkVirtualScrollViewport,
+  ],
   templateUrl: './playlist-page.html',
   styleUrl: './playlist-page.scss',
 })
@@ -39,5 +40,9 @@ export class PlaylistPage {
   constructor() {
     this.store.loadBooks();
     this.playlistId = this.route.snapshot.paramMap.get('id');
+  }
+
+  trackById(index: number, track: PlaylistTrack) {
+    return track.id;
   }
 }
